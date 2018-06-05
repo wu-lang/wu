95739ef31f755c45802f8373f24c4de1fbc872fe
Emile Yvan Boutet Livoff <emilelivoff@gmail.com>
2018-06-05 18:28:44 +0200

    [main] add file and folder transpiling from the cli

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ use wu::generator::Generator;
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::path::Path;
 
 use rustyline::error::ReadlineError;
 
@@ -79,7 +80,7 @@ fn repl() {
 
 
 
-fn run(content: &str, file: &str) {
+fn run(content: &str, file: &str) -> Option<String> {
   let source = Source::from(file, content.lines().map(|x| x.into()).collect::<Vec<String>>());
   let lexer  = Lexer::default(content.chars().collect(), &source);
 
@@ -89,7 +90,7 @@ fn run(content: &str, file: &str) {
     if let Ok(token) = token_result {
       tokens.push(token)
     } else {
-      return
+      return None
     }
   }
 
@@ -105,18 +106,15 @@ fn run(content: &str, file: &str) {
         Ok(_) => {
           let mut generator = Generator::new(&mut visitor);
 
-          println!("{}", generator.generate(&ast).unwrap())
+          generator.generate(&ast).ok()
         },
-        _ => ()
+        _ => None
       }
     },
 
-    _ => (),
+    _ => None,
   }
 }
-
-
-
 
 fn test() {
   let test0 = r"
@@ -196,17 +194,50 @@ a = loop {
   "#;
 }
 
+fn transpile_file(path: &Path) {
+  let mut content = String::new();
+  File::open(path).expect("File not found")
+                  .read_to_string(&mut content)
+                  .expect("Couldn't read the file");
+
+  if let Some(code) = run(&content, path.to_str().unwrap()) {
+    let lua_path = path.with_file_name({
+      let mut name = path.file_stem()
+                         .unwrap_or_else(|| path.file_name().unwrap())
+                         .to_os_string();
+      name.push(".lua");
+      name
+    });
+
+    let mut lua_file = File::create(lua_path).expect("Can't create file");
+    lua_file.write_all(code.as_bytes()).expect("Can't write to file")
+  }
+  println!("Transpiled \"{}\"", path.to_string_lossy());
+}
+
 fn main() {
   let args = env::args().collect::<Vec<String>>();
 
   if args.len() > 1 {
-    let mut f = File::open(&args[1]).expect("file not found");
+    let path = Path::new(&args[1]);
 
-    let mut content = String::new();
-
-    f.read_to_string(&mut content);
-
-    run(&content, &args[1])
+    if path.exists() {
+      if path.is_file() {
+        //File
+        transpile_file(&path)
+      } else if path.is_dir() {
+        //Directory
+        for file in path.read_dir().unwrap() {
+          if let Ok(file) = file {
+            if file.path().to_str().map(|s| s.ends_with(".wu")).unwrap_or(false) && file.path().is_file() {
+              transpile_file(&file.path())
+            }
+          }
+        }
+      }
+    } else {
+      panic!("File doesn't exist")
+    }
   } else {
     repl()
   }
